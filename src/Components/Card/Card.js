04c1e2c7a8d00e330ddd0cb3e025b78c979ce2b8
vickyraiman <vickyy.raiman@gmail.com>
2026-04-08 19:28:29 -0300

    favoritos

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,31 +1,63 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
-function Card(props) {
-    let rutaDetalle = "";
-    if(props.tipo === 'pelicula'){
-        rutaDetalle = `/pelicula/${props.id}`;
-    } else if(props.tipo === 'serie'){
-        rutaDetalle = `/serie/${props.id}`;
+class Card extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            esFavorito: false,
+        };
     }
 
-    return(
-        <article className="single-card-movie">
-                <img src={"https://image.tmdb.org/t/p/w342/" + props.imagen} className="card-img-top"
-                    alt={props.titulo}/>
+    agregarFavoritos() {
+
+        let arrayFavoritosPelicula = JSON.parse(localStorage.getItem("favoritosPelicula"))
+        let arrayFavoritosSerie = JSON.parse(localStorage.getItem("favoritosSerie"))
+        this.setState({
+            esFavorito: !this.state.esFavorito,
+        });
+
+        if (this.props.tipo === "pelicula") {
+            if (arrayFavoritosPelicula === null) {
+                arrayFavoritosPelicula = [];
+            }
+            arrayFavoritosPelicula.push(this.props.id);
+            localStorage.setItem("favoritosPelicula", JSON.stringify(arrayFavoritosPelicula));
+        } else if (this.props.tipo === "serie") {
+            if (arrayFavoritosSerie === null) {
+                arrayFavoritosSerie = [];
+            }
+            arrayFavoritosSerie.push(this.props.id);
+            localStorage.setItem("favoritosSerie", JSON.stringify(arrayFavoritosSerie));
+        }
+    }
+
+    render() {
+        let rutaDetalle = "";
+        if (this.props.tipo === 'pelicula') {
+            rutaDetalle = `/pelicula/${this.props.id}`;
+        } else if (this.props.tipo === 'serie') {
+            rutaDetalle = `/serie/${this.props.id}`;
+        }
+
+        return (
+            <article className="single-card-movie">
+                <img src={"https://image.tmdb.org/t/p/w342/" + this.props.imagen} className="card-img-top"
+                    alt={this.props.titulo} />
 
                 <div className="cardBody">
-                    <h5 className="card-title">{props.titulo}</h5>
-                    <p className="card-text">{props.descripcion}</p>
+                    <h5 className="card-title">{this.props.titulo}</h5>
+                    <p className="card-text">{this.props.descripcion}</p>
                     <Link to={rutaDetalle}>
-                    <button className="btn btn-primary">Ver Más</button>
+                        <button className="btn btn-primary">Ver Más</button>
                     </Link>
-                    <a href="" className="btn alert-primary">🩶</a>
+                    <button onClick={() => this.agregarFavoritos()} className="btn btn-secondary">
+                        {this.state.esFavorito ? "❤️" : "🩶"}
+                    </button>
                 </div>
-        </article>
-        
-        
-    )
+            </article>
+        );
+    }
 }
 
 export default Card;
